5fe82a67406f29ad4f9b93a27f31cc7660117a88
Jason Kurian <jasonk@nulogy.com>
2017-11-28 18:38:37 -0500

    chore: update all dependencies and test against Node LTS
    BREAKING: upgrade default-require-extensions, dropping support for node < 4

--- test/throws-with-bad-input.js
+++ test/throws-with-bad-input.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import appendTransform from '../';
 
 test('throws if getter but no setter', t => {
-	var extensions = {
+	const extensions = {
 		get '.js'() {
 			return () => t.fail();
 		}
@@ -15,7 +15,7 @@ test('throws if getter but no setter', t => {
 });
 
 test('throws if setter but no getter', t => {
-	var extensions = { // eslint-disable-line accessor-pairs
+	const extensions = { // eslint-disable-line accessor-pairs
 		set '.js'(foo) {
 			t.fail();
 		}
@@ -28,7 +28,7 @@ test('throws if setter but no getter', t => {
 });
 
 test('throws if not configurable', t => {
-	var extensions = {};
+	const extensions = {};
 
 	Object.defineProperty(extensions, '.js', {
 		value: () => t.fail(),
